5ce6883d264734349d60bc5bf38c4a78e4397708
zphoenixz <ramiro.valdez@phoenixhall.co>
2018-01-05 09:38:04 -0400

    mas o menos

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var materias = [
 app.use(routear(), ruta_materia);
 function routear(){
   for (i = 0; i < materias.length; ++i) {
-    if ( document.URL == materias[i]){
+    if ( document.URL.split('/')[2] == materias[i]){
         console.log("ire a url "+materias[i]);
         return "/"+materias[i];
     }
