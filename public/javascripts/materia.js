57b33a65c624c9875bfeea4ff67251e11fc1a4a5
zphoenixz <ramiro.valdez@phoenixhall.co>
2018-01-05 08:57:08 -0400

    mas o menos

--- public/javascripts/materia.js
+++ public/javascripts/materia.js
@@ -1,21 +1,5 @@
 
-/*
-function addLoadEvent(func) {  
-    var oldonload = window.onload;  
-    if (typeof window.onload != 'function') {  
-      window.onload = func;  
-    } else {  
-      window.onload = function() {  
-        if (oldonload) {  
-          oldonload();  
-        }  
-        func();  
-      }  
-    }  
-}  	
-addLoadEvent(function() { 
-  document.getElementById('tit').innerHTML = GetMat();
-});*/
+
 
 function onload(){
     document.getElementById("myURL").disabled = true;
