ab284a220eefcbe1f8b7172ac2250bfe59603cd3
zphoenixz <ramiro.valdez@phoenixhall.co>
2018-01-05 08:12:10 -0400

    mal

--- public/javascripts/materia.js
+++ public/javascripts/materia.js
@@ -1,13 +1,14 @@
 var NMateria = "Sin Nombre";
-
+var aux;
 function set_NMateria(nombre){
     NMateria = nombre;
+    aux = NMateria;
     console.log("Supuestamente cambie a "+NMateria);
     return NMateria;
 }
 
 function onload(){
-    console.log("La materia cargada es "+NMateria);
+    console.log("La materia cargada es "+aux);
     //document.getElementsByName('tit')[0].value = NMateria;
     
 }
